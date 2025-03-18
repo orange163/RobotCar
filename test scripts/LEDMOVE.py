6ffd50715a4e56bb6e6a8676f504fe40abc64f0c
orange163 <46548521+orange163@users.noreply.github.com>
2025-03-18 15:10:24 -0400

    Create LEDMOVE.py

---
+++ test scripts/LEDMOVE.py
@@ -0,0 +1,12 @@
+###################### pin 4
+p4.freq(1000)
+print(f"Frequence: {p4.freq()} Hz")
+print(f"Prescaler: {p4.prescaler()}")
+print(f"Period: {p4.period()}")
+p4.pulse_width_percent(100)
+##################### pin 5
+p5.freq(1000)
+print(f"Frequence: {p5.freq()} Hz")
+print(f"Prescaler: {p5.prescaler()}")
+print(f"Period: {p5.period()}")
+p5.pulse_width_percent(100)
